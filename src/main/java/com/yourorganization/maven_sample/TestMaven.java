1573bac8a8615123ab5f40d678ef0466b9df9d8e
Andriy31193 <andriycreator@gmail.com>
2023-09-28 16:52:13 +0000

    h

--- src/main/java/com/yourorganization/maven_sample/TestMaven.java
+++ src/main/java/com/yourorganization/maven_sample/TestMaven.java
@@ -6,37 +6,138 @@ import com.google.common.collect.Multimap;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
 
+import java.time.LocalDateTime;
+import java.util.Random;
+
 
 public class TestMaven {
     public static void main(String[] args) {
-        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi");
-        fruits.forEach(System.out::println);
 
-        List<String> reverseFruits = Lists.reverse(fruits);
-        reverseFruits.forEach(System.out::println);
+        //TASK 1.3
+        // TASK 1.3 ///////////
 
 
-        Multimap<String, String> map = ArrayListMultimap.create();
-        map.put("key", "firstValue");
-        map.put("key", "secondValue");
-        System.out.println(map);
+//        String jdbcUrl = "jdbc:postgresql://localhost:5437/test-db";
+//        String username = "sa";
+//        String password = "admin";
+//
+//        try {
+//            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+//            Random random = new Random();
+//
+//            while (true) {
+//                String message;
+//                String type;
+//                boolean processed = false;
+//
+//                if (random.nextBoolean()) {
+//                    message = "Новое сообщение от " + LocalDateTime.now();
+//                    type = "INFO";
+//                } else {
+//                    message = "Произошла ошибка в " + LocalDateTime.now();
+//                    type = "WARN";
+//                }
+//
+//                String insertQuery = "INSERT INTO notice (message, type, processed) VALUES (?, ?, ?)";
+//
+//                try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+//                    preparedStatement.setString(1, message);
+//                    preparedStatement.setString(2, type);
+//                    preparedStatement.setBoolean(3, processed);
+//                    preparedStatement.executeUpdate();
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Thread.sleep(1000);
+//            }
+//        } catch (SQLException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
 
-        Properties prop = new Properties();
 
-        try (InputStream resourceAsStream = TestMaven.class.getClassLoader()
-        .getResourceAsStream("config.properties")) {
-            prop.load(Objects.requireNonNull(resourceAsStream));
-        } catch (IOException e)
-    {
-        System.err.println("Unable to load properties file : confing.properties");
-    }
-        System.out.println(prop.get("props.local.hello"));
-        System.out.println(prop.get("props.mvn.hello"));
+        // TASK 1.4
+//        String jdbcUrl = "jdbc:postgresql://localhost:5437/test-db";
+//        String username = "sa";
+//        String password = "admin";
+//
+//        try {
+//            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+//
+//            while (true) {
+//                String selectQuery = "SELECT id, message FROM notice WHERE type = 'INFO' AND processed = false";
+//
+//                try (PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+//                    ResultSet resultSet = preparedStatement.executeQuery();
+//
+//                    while (resultSet.next()) {
+//                        int id = resultSet.getInt("id");
+//                        String message = resultSet.getString("message");
+//
+//                        System.out.println("Received INFO message: " + message);
+//
+//                        String deleteQuery = "DELETE FROM notice WHERE id = ?";
+//                        try (PreparedStatement deleteStatement = connection.prepareStatement(deleteQuery)) {
+//                            deleteStatement.setInt(1, id);
+//                            deleteStatement.executeUpdate();
+//                        }
+//                    }
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Thread.sleep(1000);
+//            }
+//        } catch (SQLException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
+
+
+
+
+        // TASK 1.5
+
+
+//        String jdbcUrl = "jdbc:postgresql://localhost:5437/test-db";
+//        String username = "sa";
+//        String password = "admin";
+//
+//        try {
+//            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+//
+//            while (true) {
+//                String selectQuery = "SELECT id, message FROM notice WHERE type = 'WARN' AND processed = false";
+//
+//                try (PreparedStatement preparedStatement = connection.prepareStatement(selectQuery)) {
+//                    ResultSet resultSet = preparedStatement.executeQuery();
+//
+//                    while (resultSet.next()) {
+//                        int id = resultSet.getInt("id");
+//                        String message = resultSet.getString("message");
+//
+//                        System.out.println("Received WARN message: " + message);
+//
+//                        String updateQuery = "UPDATE notice SET processed = false WHERE id = ?";
+//                        try (PreparedStatement updateStatement = connection.prepareStatement(updateQuery)) {
+//                            updateStatement.setInt(1, id);
+//                            updateStatement.executeUpdate();
+//                        }
+//                    }
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Thread.sleep(1000);
+//            }
+//        } catch (SQLException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
